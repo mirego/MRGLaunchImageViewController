baefad30a8187490e6d1333c9a152ce97f0b118b
Francois Lambert <flambert@mirego.com>
2015-09-18 17:40:51 -0400

    Modernize pod

--- Example/Pods/Pods-MRGLaunchImageViewController-MRGLaunchImageViewController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MRGLaunchImageViewController_MRGLaunchImageViewController : NSObject
-@end
-@implementation PodsDummy_Pods_MRGLaunchImageViewController_MRGLaunchImageViewController
-@end
---
+++ Example/Pods/Target Support Files/MRGLaunchImageViewController/MRGLaunchImageViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MRGLaunchImageViewController : NSObject
+@end
+@implementation PodsDummy_MRGLaunchImageViewController
+@end
